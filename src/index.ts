1077d4b182e8dd875516427d9611f3ad82b89fd5
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-06-28 00:07:43 +0200

    Accessed firebase api

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as functions from 'firebase-functions';
+import { AppModule } from './app.module';
+const expressServer = express();
+
+const createFunction = async (expressInstance): Promise<void> => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  await app.init();
+};
+
+export const api = functions.https.onRequest(async (request, response) => {
+  await createFunction(expressServer)
+    .then((v) => console.log('nest ready'))
+    .catch((err) => console.log('Nest broken', err));
+  expressServer(request, response);
+});
