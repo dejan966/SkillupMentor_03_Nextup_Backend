a13996ba1ef01c8e5fb8c11addaac546f0a3d06c
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-09-17 13:34:52 +0200

    Firebase connection with service account

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(AuthGuard('jwt'))
+  @Get('/hello')
+  getHello(@Req() request: Request): string {
+    console.log('User', JSON.stringify(request['user']));
+    return 'Hello ' + JSON.stringify(request['user']);
+  }
+
+  @Get('usersF')
+  async getAllUsers(){
+    return this.appService.getAllUsers();
+  }
+
+  @Get('usersDB')
+  async getAllUsersFromDB(){
+    return this.appService.getAllUsersFromDB();
+  }
+  
+  @Get('/')
+  getHelloWord(): string {
+    return this.appService.getHelloWorld();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import * as admin from "firebase-admin";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getAllUsers(){
+    return admin.auth().listUsers();
+  }
+
+  async getAllUsersFromDB(){
+    const db = admin.firestore();
+    const snapshot = await db.collection("users").get();
+    const data = [];
+    snapshot.forEach(doc => {
+      data.push({ id: doc.id, ... doc.data()});
+    });
+    return data;
+  }
+
+  getHelloWorld(): string {
+    return 'Hello World';
   }
 }
