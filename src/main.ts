85a370eaa1db055885c50f8d14139ec9f060f056
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-09-17 16:10:46 +0200

    Nest with firebase integration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as admin from "firebase-admin";
+const expressServer = express();
+require('dotenv').config();
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+const createFunction = async (expressInstance): Promise<void> => {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
-}
-bootstrap();
+};
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    privateKey: process.env.F_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.F_CLIENT_EMAIL,
+    projectId: process.env.F_PROJECT_ID
+  }),
+  databaseURL: process.env.F_DATABASE_URL
+});
+
+createFunction(expressServer);
