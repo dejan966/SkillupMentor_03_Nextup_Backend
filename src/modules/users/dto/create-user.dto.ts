2d636e1924b0a7e1470cb0238f7fa18e999701a6
dejan966 <59951914+dejan966@users.noreply.github.com>
2024-01-18 16:10:42 +0100

    User dto

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,33 @@
-export class CreateUserDto {}
+import { IsOptional, IsNotEmpty, IsEmail, Matches } from "class-validator";
+import { Match } from "decorators/match.decorator";
+
+export class CreateUserDto {
+  @IsOptional()
+  first_name?: string;
+
+  @IsOptional()
+  last_name?: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @Matches(/^(?=.*\d)[A-Za-z.\s_-]+[\w~@#$%^&+=`|{}:;!.?"()[\]-]{6,}/, {
+    message:
+      'Password must have atleast one number, lower or upper case letter and it has to be longer than five characters',
+  })
+  password: string;
+
+  @IsNotEmpty()
+  @Match(CreateUserDto, (field) => field.password, {
+    message: 'Passwords do not match',
+  })
+  confirm_password: string;
+
+  @IsOptional()
+  refresh_token?: string;
+
+  @IsOptional()
+  password_token?: string;
+}
