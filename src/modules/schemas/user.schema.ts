bfc551b02e2fcf7487ac38900782efba7416714b
dejan966 <59951914+dejan966@users.noreply.github.com>
2024-01-18 13:00:50 +0100

    user schema and POST

--- src/modules/schemas/user.schema.ts
+++ src/modules/schemas/user.schema.ts
@@ -1,12 +1,34 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Exclude } from 'class-transformer';
 import { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema()
 export class User {
-  @Prop()
+  @Prop({ nullable: true })
   first_name: string;
+  
+  @Prop({ nullable: true })
+  last_name: string;
+  
+  @Prop({ required: true })
+  email: string;
+  
+  @Prop({ required: true })
+  @Exclude()
+  password: string;
+
+  @Prop({ default: 'default_profile.svg' })
+  avatar: string;
+
+  @Prop({ nullable: true, default: null })
+  @Exclude()
+  refresh_token: string;
+
+  @Prop({ nullable: true, default: null })
+  @Exclude()
+  password_token: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -8,27 +8,27 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
+  create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
   @Get()
-  async findAll() {
+  findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
-  async findById(@Param('id') id: string) {
+  findById(@Param('id') id: string) {
     return this.usersService.findById(id);
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
+  remove(@Param('id') id: string) {
     return this.usersService.remove(id);
   }
 }
