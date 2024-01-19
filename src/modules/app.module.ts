751efc7a4070caf74efc6b77eebcd3b111a18309
dejan966 <59951914+dejan966@users.noreply.github.com>
2024-01-19 19:17:00 +0100

    resources for events generated

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { AuthModule } from './auth/auth.module';
     MongooseModule.forRoot(process.env.M_DATABASE_URL),
     AuthModule,
     UsersModule,
+    EventsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
