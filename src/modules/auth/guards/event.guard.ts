fbfb4be9a8c5da142d66ce9fc4a38ccf382972db
dejan966 <ultimate24208@gmail.com>
2024-11-11 18:33:39 +0100

    iterator => created_event - event.guard

--- src/modules/auth/guards/event.guard.ts
+++ src/modules/auth/guards/event.guard.ts
@@ -7,8 +7,8 @@ export class EventGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const { user, params } = context.switchToHttp().getRequest();
-    for (const iterator of user.created_events) {
-      if (iterator._id.toString() === params.id || user.role.name === 'ADMIN')
+    for (const created_event of user.created_events) {
+      if (created_event._id.toString() === params.id || user.role.name === 'ADMIN')
         return true;
     }
     return false;
