b692a2f4a84c840561db853b61e906afbaf7a71d
dejan966 <ultimate24208@gmail.com>
2024-11-11 16:53:33 +0100

    momentjs date in logs

--- src/library/Logging.ts
+++ src/library/Logging.ts
@@ -1,10 +1,15 @@
+import moment from 'moment';
+
 const chalk = import('chalk').then((m) => m.default);
+var momentDate = moment();
+momentDate.locale('sl');
+const date = momentDate.format("L, LTS");
 
 export default class Logging {
   public static info = async (args: any) =>
     console.log(
       (await chalk).blue(
-        `[${new Date().toLocaleString()}] [INFO]`,
+        `[${date}] [INFO]`,
         typeof args === 'string' ? (await chalk).blueBright(args) : args,
       ),
     );
@@ -12,7 +17,7 @@ export default class Logging {
   public static warn = async (args: any) =>
     console.log(
       (await chalk).yellow(
-        `[${new Date().toLocaleString()}] [INFO]`,
+        `[${date}] [INFO]`,
         typeof args === 'string' ? (await chalk).yellowBright(args) : args,
       ),
     );
@@ -20,7 +25,7 @@ export default class Logging {
   public static error = async (args: any) =>
     console.log(
       (await chalk).red(
-        `[${new Date().toLocaleString()}] [INFO]`,
+        `[${date}] [INFO]`,
         typeof args === 'string' ? (await chalk).redBright(args) : args,
       ),
     );
