ea8208a47fe509bbb0456d6bacbe177cb992bc6c
dejan966 <ultimate24208@gmail.com>
2026-01-25 19:08:26 +0100

    vercel

--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,61 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'modules/app.module';
-import express from 'express';
 import { ValidationPipe } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import path from 'path';
+import serverlessExpress from '@vendia/serverless-express';
+import express from 'express';
 import cookieParser from 'cookie-parser';
 import * as admin from 'firebase-admin';
 
+let server: any;
+
 async function bootstrap() {
-  const expressInstance = express();
+  if (server) return server;
+
+  const expressApp = express();
   const app = await NestFactory.create(
     AppModule,
-    new ExpressAdapter(expressInstance),
+    new ExpressAdapter(expressApp),
   );
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [process.env.FRONTEND],
     credentials: true,
   });
 
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
 
-  const dirname = path.resolve();
-  app.use('/uploads', express.static(path.join(dirname, '/uploads')));
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Nextup')
+      .setDescription('This is the Nextup app')
+      .setVersion('1.0.0')
+      .build();
 
-  const config = new DocumentBuilder()
-    .setTitle('Nextup')
-    .setDescription('This is the Nextup app')
-    .setVersion('1.0.0')
-    .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
+  }
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/', app, document);
+  await app.init();
 
-  const PORT = process.env.PORT || 8080;
-  await app.listen(PORT);
+  server = serverlessExpress({ app: expressApp });
+  return server;
 }
 
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.F_PROJECT_ID,
-    clientEmail: process.env.F_CLIENT_EMAIL,
-    privateKey: process.env.F_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  }),
-  databaseURL: process.env.F_DATABASE_URL,
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    }),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+  });
+}
 
-bootstrap();
+export default async function handler(req, res) {
+  const server = await bootstrap();
+  return server(req, res);
+}
