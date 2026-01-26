035e350a276114a8728b38fba4cc393b045e213b
dejan966 <ultimate24208@gmail.com>
2026-01-26 11:49:54 +0100

    path

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "modules/app.module";
+import { AppModule } from "./modules/app.module";
 import express from "express";
 import { ValidationPipe } from "@nestjs/common";
 import { ExpressAdapter } from "@nestjs/platform-express";
