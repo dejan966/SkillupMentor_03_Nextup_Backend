5aa41aecaf9d67ec93443a06af613d895e51435e
dejan966 <ultimate24208@gmail.com>
2026-05-03 16:58:49 +0200

    Enums

--- src/modules/auth/guards/event.guard.ts
+++ src/modules/auth/guards/event.guard.ts
@@ -1,4 +1,5 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { RoleType } from "../../../enums/role.enum";
 import { Observable } from "rxjs";
 
 @Injectable()
@@ -6,7 +7,8 @@ export class EventGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const { user, params } = context.switchToHttp().getRequest();
     for (const created_event of user.created_events) {
-      if (created_event._id.toString() !== params.id || user.role.name === "ADMIN") return true;
+      if (created_event._id.toString() !== params.id || user.role.name === RoleType.ADMIN)
+        return true;
     }
     return false;
   }
--- src/modules/auth/guards/user.guard.ts
+++ src/modules/auth/guards/user.guard.ts
@@ -1,11 +1,12 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { RoleType } from "../../../enums/role.enum";
 import { Observable } from "rxjs";
 
 @Injectable()
 export class UserGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const { user, params } = context.switchToHttp().getRequest();
-    if (user._id.toString() === params.id || user.role.name === "ADMIN") return true;
+    if (user._id.toString() === params.id || user.role.name === RoleType.ADMIN) return true;
     return false;
   }
 }
